909a2a6915628951fb9d0cf9fc3adef90232bdde
Justin Prim <justinprim140969@gmail.com>
2024-01-10 12:50:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John"; 
+            int age  = 10;
+            bool isTall = false;
+            double weight = 25.6; 
+
+            Console.WriteLine("Hi "+name+", you are "+age+" your weight is "+ weight);
+            Console.WriteLine("True or false, are you tall: "+ isTall);
+
+            Console.ReadLine();
+        }
+    }
+}
